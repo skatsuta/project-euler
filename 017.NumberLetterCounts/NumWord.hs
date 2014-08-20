753a85be2ad69bfba2c8cccfd995ad389f0772bf
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-08-21 03:13:54 +0900

    Work in progress on Problem 17

--- 017.NumberLetterCounts/NumWord.hs
+++ 017.NumberLetterCounts/NumWord.hs
@@ -1,5 +1,18 @@
-module NumWord (oneDigitToWord) where
+module NumWord (numLetterCount) where
 
-oneDigitToWord :: Int -> String
-oneDigitToWord x
-  | x < 1 || x > 9 = error "One digit Only"
+numLetterCount :: Int -> Int
+numLetterCount 1 = 3  -- one
+numLetterCount 2 = 3  -- two
+numLetterCount 3 = 5  -- three
+numLetterCount 4 = 4  -- four
+numLetterCount 5 = 4  -- five
+numLetterCount 6 = 3  -- six
+numLetterCount 7 = 5  -- seven
+numLetterCount 8 = 5  -- eight
+numLetterCount 9 = 4  -- nine
+numLetterCount 10 = 3 -- ten
+numLetterCount 11 = 6 -- eleven
+numLetterCount 12 = 6 -- twelve
+numLetterCount 13 = 8 -- thirteen
+numLetterCount 14 = 8 -- fourteen
+numLetterCount 15 = 
