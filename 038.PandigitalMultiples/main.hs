b8c3a10b193261f3166f504162657bba143af157
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-08-24 04:13:15 +0900

    Solve Problem 38

--- 038.PandigitalMultiples/main.hs
+++ 038.PandigitalMultiples/main.hs
@@ -2,10 +2,11 @@ import Data.List
 import Control.Applicative
 
 main :: IO ()
-main = print $ concatProduct 2 5
+main = print $ maximum $ map (read :: String -> Int) $ filter isPandigital $ takeWhile (\xs -> length xs < 10) $ concatProduct <$> [2..] <*> [1..10000]
 
 isPandigital :: String -> Bool
-isPandigital n = n `elem` pandigitals where pandigitals = permutations ['1'..'9']
+isPandigital [] = True
+isPandigital (x:xs) = x /= '0' && notElem x xs && isPandigital xs
 
 concatProduct :: Int -> Int -> String
-concatProduct int n = concatMap show $ (*) <$> [1..n] <*> [int]
+concatProduct n int = concatMap show $ (*) <$> [1..n] <*> [int]
