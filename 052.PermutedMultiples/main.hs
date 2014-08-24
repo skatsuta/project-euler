0ec9cf27bb05370ff8d40af05b7bbc18df9b675e
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-08-24 16:23:36 +0900

    Work in progress on Problem 52

--- 052.PermutedMultiples/main.hs
+++ 052.PermutedMultiples/main.hs
@@ -1,7 +1,16 @@
 import Data.List
+import Control.Applicative
 
 main :: IO ()
-main = print $ permutablyEqual 2423 4321
+main = print $ allPermutablyEqual [123, 231, 312]
 
 permutablyEqual :: Int -> Int -> Bool
-permutablyEqual x y = show x `elem` permutations (show y)
+permutablyEqual x y = permutablyEqual' (show x) (show y)
+  where permutablyEqual' [] _ = True
+        permutablyEqual' _ [] = True
+        permutablyEqual' (x':xs) y' = x' `elem` y' && permutablyEqual' xs y'
+
+allPermutablyEqual :: [Int] -> Bool
+allPermutablyEqual [] = True
+allPermutablyEqual [_] = True
+allPermutablyEqual (x:y:xs) = permutablyEqual x y && allPermutablyEqual (y:xs)
