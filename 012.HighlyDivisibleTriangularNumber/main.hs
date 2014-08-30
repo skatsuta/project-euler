c691960e0566ccce76b60e12b69909d426ef47da
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-08-31 05:35:34 +0900

    Solve Problem 12

--- 012.HighlyDivisibleTriangularNumber/main.hs
+++ 012.HighlyDivisibleTriangularNumber/main.hs
@@ -1,15 +1,10 @@
 import Data.List
-import System.Environment
+import Math.NumberTheory.Primes.Factorisation
+import Data.Maybe
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let n = read $ head args :: Integer
-  print $ find (\xs -> length xs >= 500) $ map divisors $ triSeries n
+main = print $ fst $ fromJust $ find (\t -> snd t >= 500) $ map (\x -> (x, divisorCount x)) triSeries
 
-divisors :: Integer -> [Integer]
-divisors n = 1 : filter ((== 0) . rem n) [2..n `div` 2]
+triSeries :: [Integer]
+triSeries = scanl (+) 1 [2..]
 
-triSeries :: Integer -> [Integer]
-triSeries n = scanl (+) 1 [2..n]
---triSeries n = map (\m -> m * (m + 1) `div` 2) [1..n]
